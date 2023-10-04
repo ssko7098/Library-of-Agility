eba366f5ba7490d6143a04beb44895f0758b75f8
ssko7098 <ssko7098@uni.sydney.edu.au>
2023-10-05 10:41:16 +1100

    Created User class

---
+++ app/src/main/java/Lab29/Huaicheng/Group1/A2/User.java
@@ -0,0 +1,67 @@
+package Lab29.Huaicheng.Group1.A2;
+
+public class User {
+
+    private String username;
+    private String password;
+    private Long phoneNumber;
+    private String emailAddress;
+    private String fullName;
+    private boolean isAdmin = false;
+
+    public User(String username, String password, Long phoneNumber, String emailAddress, String fullname) {
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.fullName = fullname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
