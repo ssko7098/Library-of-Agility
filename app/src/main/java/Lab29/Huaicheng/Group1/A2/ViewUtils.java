18c833180fa9ed0694689aedfdd53cd0071530e7
ssko7098 <ssko7098@uni.sydney.edu.au>
2023-10-05 14:30:15 +1100

    Moved UI logic to ViewUtils class

--- app/src/main/java/Lab29/Huaicheng/Group1/A2/ViewUtils.java
+++ app/src/main/java/Lab29/Huaicheng/Group1/A2/ViewUtils.java
@@ -1,5 +1,12 @@
 package Lab29.Huaicheng.Group1.A2;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ViewUtils {
@@ -72,5 +79,28 @@ public class ViewUtils {
             System.out.println("Invalid input - must be y or n");
 
         } while (true);
+    }
+
+    public static void viewAllUsers() {
+        JSONParser parser = new JSONParser();
+        JSONArray users = null;
+
+        try {
+            users = (JSONArray) parser.parse(new FileReader("users.json"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        for(int i=0; i<users.size(); i++) {
+            JSONObject user = (JSONObject) users.get(i);
+
+            for(Object entry: user.entrySet()) {
+                System.out.println(entry.toString());
+            }
+
+            System.out.println("\n");
+        }
     }
 }
