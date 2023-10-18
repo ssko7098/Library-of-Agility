b3f42175232f4e423dd850197c4ba98ab95d66b8
ssko7098 <ssko7098@uni.sydney.edu.au>
2023-10-18 23:09:10 +1100

    added file uploader window with dialog

---
+++ app/src/main/java/Lab29/Huaicheng/Group1/A2/FileUploaderWithDialog.java
@@ -0,0 +1,28 @@
+package Lab29.Huaicheng.Group1.A2;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FileUploaderWithDialog {
+
+    public static String chooseFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+        fileChooser.setFileFilter(filter);
+
+        int returnValue = fileChooser.showOpenDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            return selectedFile.getPath();
+        }
+
+        return null;
+    }
+}
