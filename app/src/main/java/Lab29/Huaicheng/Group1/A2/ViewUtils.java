9965458f760cb9b656b2547391a093c41b34c1ad
ssko7098 <ssko7098@uni.sydney.edu.au>
2023-10-25 22:08:24 +1100

    fixed more bugs in Ryan's search function

--- app/src/main/java/Lab29/Huaicheng/Group1/A2/ViewUtils.java
+++ app/src/main/java/Lab29/Huaicheng/Group1/A2/ViewUtils.java
@@ -350,6 +350,11 @@ public class ViewUtils {
                 }
             }
         }
+
+        if(scrollDetails.size() == 1) {
+            return null;
+        }
+
         return scrollDetails.toArray(new String[0]);
     }
 
